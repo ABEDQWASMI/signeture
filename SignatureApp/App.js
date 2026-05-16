3a429620cb69b182d44743af1bf3e56c5ae1f2ad
ABEDQWASMI <Abodqwasmi.16@gmail.com>
2026-05-16 16:18:19 +0300

    fix: add SafeAreaProvider to App.js - fixes black screen after splash on web

--- SignatureApp/App.js
+++ SignatureApp/App.js
@@ -1,14 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppProvider } from './src/context/AppContext';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <AppProvider>
-      <StatusBar style="light" backgroundColor="#000000" />
-      <AppNavigator />
-    </AppProvider>
+    <SafeAreaProvider>
+      <AppProvider>
+        <StatusBar style="light" backgroundColor="#000000" />
+        <AppNavigator />
+      </AppProvider>
+    </SafeAreaProvider>
   );
 }
